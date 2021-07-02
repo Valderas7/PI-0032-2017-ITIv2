4c876384d3e8365007880912160067ac59418c74
Valderas7 <valderas999@hotmail.com>
2021-07-02 09:22:18 +0200

    Todo ordenaditto

---
+++ cnv&snv_mutations/cnv/inferencia/load_best_model/load_best_model_cnv_only_image.py
@@ -0,0 +1,63 @@
+import shelve # datos persistentes
+import pandas as pd
+import numpy as np
+import seaborn as sns # Para realizar gráficas sobre datos
+import matplotlib.pyplot as plt
+import cv2 #OpenCV
+import imageio
+import imgaug as ia
+import imgaug.augmenters as iaa
+import glob
+import imblearn
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.models import load_model
+from tensorflow.keras import layers
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.layers import Input # Para instanciar tensores de Keras
+from functools import reduce # 'reduce' aplica una función pasada como argumento para todos los miembros de una lista.
+from sklearn.model_selection import train_test_split # Se importa la librería para dividir los datos en entreno y test.
+from sklearn.preprocessing import MinMaxScaler # Para escalar valores
+from sklearn.metrics import confusion_matrix # Para realizar la matriz de confusión
+
+model = load_model('/home/avalderas/img_slides/cnv&snv_mutations/cnv/training_codes/model_cnv_image_myc_epoch07.h5')
+
+test_image_data = np.load('/home/avalderas/img_slides/cnv&snv_mutations/cnv/inferencia/test_data/image_only/test_image.npy')
+test_labels = np.load('/home/avalderas/img_slides/cnv&snv_mutations/cnv/inferencia/test_data/image_only/test_labels.npy')
+
+""" Una vez entrenado el modelo, se puede evaluar con los datos de test y obtener los resultados de las métricas
+especificadas en el proceso de entrenamiento. En este caso, se decide mostrar los resultados de la 'loss', la exactitud,
+la sensibilidad y la precisión del conjunto de datos de validación."""
+# @evaluate: Devuelve el valor de la 'loss' y de las métricas del modelo especificadas.
+results = model.evaluate(test_image_data,test_labels, verbose = 0)
+print("\n'Loss' del conjunto de prueba: {:.2f}\n""Sensibilidad del conjunto de prueba: {:.2f}\n" 
+      "Precisión del conjunto de prueba: {:.2f}\n""Accuracy del conjunto de prueba: {:.2f} %".format((results[0]),
+                                                                                                   (results[5]),
+                                                                                                   (results[6]),
+                                                                                                   results[7] * 100))
+
+""" Por último, y una vez entrenada ya la red, también se pueden hacer predicciones con nuevos ejemplos usando el
+conjunto de datos de test que se definió anteriormente al repartir los datos. """
+# @suppress=True: Muestra los números con representación de coma fija
+# @predict: Genera predicciones para nuevas entradas
+print("\nGenera predicciones para 10 muestras")
+print("Clase de las salidas: ", test_labels[:10])
+np.set_printoptions(precision=3, suppress=True)
+print("Predicciones:\n", np.round(model.predict(test_image_data[:10])))
+
+""" Además, se realiza la matriz de confusión sobre todo el conjunto del dataset de test para evaluar la precisión de la
+red neuronal y saber la cantidad de falsos positivos, falsos negativos, verdaderos negativos y verdaderos positivos. """
+y_true = test_labels # Etiquetas verdaderas de 'test'
+y_pred = np.round(model.predict(test_image_data)) # Predicción de etiquetas de 'test'
+
+matrix = confusion_matrix(y_true, y_pred) # Calcula (pero no dibuja) la matriz de confusión
+
+group_names = ['True Neg','False Pos','False Neg','True Pos'] # Nombres de los grupos
+group_counts = ['{0:0.0f}'.format(value) for value in matrix.flatten()] # Cantidad de casos por grupo
+
+""" @zip: Une las tuplas del nombre de los grupos con la de la cantidad de casos por grupo """
+labels = [f'{v1}\n{v2}\n' for v1, v2 in zip(group_names,group_counts)]
+labels = np.asarray(labels).reshape(2,2)
+sns.heatmap(matrix, annot=labels, fmt='', cmap='Blues')
+plt.show() # Muestra la gráfica de la matriz de confusión
