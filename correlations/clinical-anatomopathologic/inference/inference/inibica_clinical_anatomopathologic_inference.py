35437f5d01109403b97ad6a2e7d017036e0c62dd
Valderas7 <valderas999@hotmail.com>
2021-10-14 12:58:09 +0200

    Inferencia TCGA e INiBICA completada en correlación de datos clínicos con datos de anatomía patológica

--- correlations/clinical-anatomopathologic/inference/inference/inibica_clinical_anatomopathologic_inference.py
+++ correlations/clinical-anatomopathologic/inference/inference/inibica_clinical_anatomopathologic_inference.py
@@ -1,31 +1,221 @@
+""" Librerías """
 import pandas as pd
 import numpy as np
 import seaborn as sns # Para realizar gráficas sobre datos
 import matplotlib.pyplot as plt
-import cv2 #OpenCV
 import glob
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.models import load_model
-from sklearn.metrics import confusion_matrix # Para realizar la matriz de confusión
+from tensorflow.keras.layers import Input # Para instanciar tensores de Keras
+from functools import reduce # 'reduce' aplica una función pasada como argumento para todos los miembros de una lista.
+from sklearn.metrics import confusion_matrix
 import itertools
 
-""" Se carga el modelo de red neuronal entrenado y los distintos datos de entrada y datos de salida guardados en formato 
-'numpy' """
+""" Se carga el Excel de los pacientes de INiBICA """
+data_inibica = pd.read_excel('/home/avalderas/img_slides/excel_genesOCA&inibica_patients/inference_inibica.xlsx',
+                             engine='openpyxl')
+
+""" Se sustituyen los valores de la columna del estado de supervivencia, puesto que se entrenaron para valores de '1' 
+para los pacientes fallecidos, al contrario que en el Excel de los pacientes de INiBICA """
+data_inibica.loc[data_inibica.Estado_supervivencia == 1, "Estado_supervivencia"] = 2
+data_inibica.loc[data_inibica.Estado_supervivencia == 0, "Estado_supervivencia"] = 1
+data_inibica.loc[data_inibica.Estado_supervivencia == 2, "Estado_supervivencia"] = 0
+
+""" Se aprecian valores nulos en la columna de 'Diagnostico Previo'. Para nos desechar estos pacientes, se ha optado por 
+considerarlos como pacientes sin diagnostico previo: """
+data_inibica['Diagnóstico_previo'].fillna(value = 0, inplace= True)
+
+""" Se crea la misma cantidad de columnas para los tipos de tumor que se creo en el conjunto de entrenamiento para 
+rellenarlas posteriormente con un '1' en las filas que corresponda, dependiendo de los tipos de tumor de los pacientes 
+de INiBICA. """
+data_inibica['Tumor_IDC'] = 0
+data_inibica['Tumor_ILC'] = 0
+data_inibica['Tumor_Metaplastic'] = 0
+data_inibica['Tumor_Mixed'] = 0
+data_inibica['Tumor_Mucinous'] = 0
+data_inibica['Tumor_Other'] = 0
+
+data_inibica.loc[(data_inibica['Tipo_tumor'] == 'Mucinous'), 'Tumor_Mucinous'] = 1
+data_inibica.loc[(data_inibica['Tipo_tumor'].str.contains("Lobular")) |
+                 (data_inibica['Tipo_tumor'] == 'Signet-ring cells lobular'), 'Tumor_ILC'] = 1
+data_inibica.loc[(data_inibica['Tipo_tumor'] == 'Invasive carcinoma (NST)') |
+                 (data_inibica['Tipo_tumor'] == 'Microinvasive carcinoma') |
+                 (data_inibica['Tipo_tumor'] == 'Signet-ring cells ductal'), 'Tumor_IDC'] = 1
+data_inibica.loc[(data_inibica['Tipo_tumor'] == 'Mixed ductal and lobular'), 'Tumor_Mixed'] = 1
+data_inibica.loc[(data_inibica['Tipo_tumor'] == 'Apocrine') | (data_inibica['Tipo_tumor'] == 'Papillary') |
+                 (data_inibica['Tipo_tumor'] == 'Tubular') | (data_inibica['Tipo_tumor'] == 'Medullary'),
+                 'Tumor_Other'] = 1
+
+""" Se elimina la columna 'Tipo_tumor', ya que ya no nos sirve, al igual que las demas columnas que no se utilizan para 
+la prediccion de metastasis. """
+data_inibica = data_inibica.drop(['Tipo_tumor'], axis = 1)
+
+""" Se crea la misma cantidad de columnas para la variable 'STAGE' que se creo en el conjunto de entrenamiento para 
+rellenarlas posteriormente con un '1' en las filas que corresponda, dependiendo de la fase de 'STAGE' de los pacientes 
+de INiBICA. """
+data_inibica['STAGE_IB'] = 0
+data_inibica['STAGE_II'] = 0
+data_inibica['STAGE_IIA'] = 0
+data_inibica['STAGE_IIB'] = 0
+data_inibica['STAGE_III'] = 0
+data_inibica['STAGE_IIIA'] = 0
+data_inibica['STAGE_IIIB'] = 0
+data_inibica['STAGE_IIIC'] = 0
+data_inibica['STAGE_X'] = 0
+
+data_inibica.loc[(data_inibica['STAGE'] == 'IB'), 'STAGE_IB'] = 1
+data_inibica.loc[(data_inibica['STAGE'] == 'IIA'), 'STAGE_IIA'] = 1
+data_inibica.loc[(data_inibica['STAGE'] == 'IIB'), 'STAGE_IIB'] = 1
+data_inibica.loc[(data_inibica['STAGE'] == 'IIIA'), 'STAGE_IIIA'] = 1
+data_inibica.loc[(data_inibica['STAGE'] == 'IIIB'), 'STAGE_IIIB'] = 1
+data_inibica.loc[(data_inibica['STAGE'] == 'IIIC'), 'STAGE_IIIC'] = 1
+
+""" Se elimina la columna 'STAGE', ya que ya no nos sirve, al igual que las demas columnas que no se utilizan para 
+la prediccion de metastasis. """
+data_inibica = data_inibica.drop(['STAGE'], axis = 1)
+
+""" Se crean la misma cantidad de columnas para los estadios T que se crearon en el conjunto de entrenamiento para 
+rellenarlas posteriormente con un '1' en las filas que corresponda, dependiendo del valor pT de los pacientes de INiBICA. """
+data_inibica['pT_T1'] = 0
+data_inibica['pT_T1B'] = 0
+data_inibica['pT_T1C'] = 0
+data_inibica['pT_T2'] = 0
+data_inibica['pT_T2B'] = 0
+data_inibica['pT_T3'] = 0
+data_inibica['pT_T4'] = 0
+data_inibica['pT_T4B'] = 0
+data_inibica['pT_T4D'] = 0
+
+data_inibica.loc[data_inibica.pT == 1, 'pT_T1'] = 1
+data_inibica.loc[data_inibica.pT == '1b', 'pT_T1B'] = 1
+data_inibica.loc[data_inibica.pT == '1c', 'pT_T1C'] = 1
+data_inibica.loc[data_inibica.pT == 2, 'pT_T2'] = 1
+data_inibica.loc[data_inibica.pT == 3, 'pT_T3'] = 1
+data_inibica.loc[data_inibica.pT == 4, 'pT_T4'] = 1
+data_inibica.loc[data_inibica.pT == '4a', 'pT_T4'] = 1
+data_inibica.loc[data_inibica.pT == '4b', 'pT_T4B'] = 1
+data_inibica.loc[data_inibica.pT == '4d', 'pT_T4D'] = 1
+
+""" Se elimina la columna 'pT', ya que ya no nos sirve """
+data_inibica = data_inibica.drop(['pT'], axis = 1)
+
+""" Se crean la misma cantidad de columnas para los estadios N que se crearon en el conjunto de entrenamiento para 
+rellenarlas posteriormente con un '1' en las filas que corresponda, dependiendo del valor pN de los pacientes de INiBICA. """
+data_inibica['pN_N1'] = 0
+data_inibica['pN_N1A'] = 0
+data_inibica['pN_N1B'] = 0
+data_inibica['pN_N1C'] = 0
+data_inibica['pN_N1MI'] = 0
+data_inibica['pN_N2'] = 0
+data_inibica['pN_N2A'] = 0
+data_inibica['pN_N3'] = 0
+data_inibica['pN_N3A'] = 0
+data_inibica['pN_N3B'] = 0
+
+data_inibica.loc[data_inibica.pN == 1, 'pN_N1'] = 1
+data_inibica.loc[data_inibica.pN == '1a', 'pN_N1A'] = 1
+data_inibica.loc[data_inibica.pN == '1b', 'pN_N1B'] = 1
+data_inibica.loc[data_inibica.pN == '1c', 'pN_N1C'] = 1
+data_inibica.loc[data_inibica.pN == '1mi', 'pN_N1MI'] = 1
+data_inibica.loc[data_inibica.pN == 2, 'pN_N2'] = 1
+data_inibica.loc[data_inibica.pN == '2a', 'pN_N2A'] = 1
+data_inibica.loc[data_inibica.pN == 3, 'pN_N3'] = 1
+data_inibica.loc[data_inibica.pN == '3a', 'pN_N3A'] = 1
+
+""" Se elimina la columna 'pN', ya que ya no nos sirve """
+data_inibica = data_inibica.drop(['pN'], axis = 1)
+
+""" Se crean la misma cantidad de columnas para los estadios M que se crearon en el conjunto de entrenamiento para 
+rellenarlas posteriormente con un '1' en las filas que corresponda, dependiendo del valor pM de los pacientes de INiBICA. """
+data_inibica['pM_M0'] = 0
+data_inibica['pM_M1'] = 0
+data_inibica['pM_MX'] = 0
+
+data_inibica.loc[(data_inibica['pM'] == 0), 'pM_M0'] = 1
+data_inibica.loc[(data_inibica['pM'] == 'X'), 'pM_MX'] = 1
+
+""" Se elimina la columna 'pM', ya que ya no nos sirve """
+data_inibica = data_inibica.drop(['pM'], axis = 1)
+
+""" Se crean la misma cantidad de columnas para la IHQ que se crearon en el conjunto de entrenamiento para rellenarlas 
+posteriormente con un '1' en las filas que corresponda, dependiendo del valor de los distintos receptores de los 
+pacientes de INiBICA. """
+data_inibica['IHQ_Basal'] = 0
+data_inibica['IHQ_Her2'] = 0
+data_inibica['IHQ_Luminal_A'] = 0
+data_inibica['IHQ_Luminal_B'] = 0
+data_inibica['IHQ_Normal'] = 0
+
+""" Se aprecian valores nulos en la columna de 'Ki-67'. Para no desechar estos pacientes, se ha optado por considerarlos 
+como pacientes con porcentaje mínimo de Ki-67: """
+data_inibica['Ki-67'].fillna(value = 0, inplace= True)
+
+""" Para el criterio de IHQ, se busca en internet el criterio para clasificar los distintos cáncer de mama en Luminal A, 
+Luminal B, Her2, Basal, etc. """
+data_inibica.loc[((data_inibica['ER'] > 0.01) | (data_inibica['PR'] > 0.01)) & ((data_inibica['Her-2'] == 0) |
+                                                                                (data_inibica['Her-2'] == '1+')) &
+                 (data_inibica['Ki-67'] < 0.14),'IHQ_Luminal_A'] = 1
+
+data_inibica.loc[(((data_inibica['ER'] > 0.01) | (data_inibica['PR'] > 0.01)) &
+                  ((data_inibica['Her-2'] == 0) | (data_inibica['Her-2'] == '1+')) & (data_inibica['Ki-67'] >= 0.14)) |
+                 (((data_inibica['ER'] > 0.01) | (data_inibica['PR'] > 0.01)) &
+                  ((data_inibica['Her-2'] == '2+') | (data_inibica['Her-2'] == '3+'))),'IHQ_Luminal_B'] = 1
+
+data_inibica.loc[(data_inibica['ER'] <= 0.01) & (data_inibica['PR'] <= 0.01) &
+                 ((data_inibica['Her-2'] == '2+') | (data_inibica['Her-2'] == '3+')),'IHQ_Her2'] = 1
+
+data_inibica.loc[(data_inibica['ER'] <= 0.01) & (data_inibica['PR'] <= 0.01) &
+                 ((data_inibica['Her-2'] == 0) | (data_inibica['Her-2'] == '1+')),'IHQ_Basal'] = 1
+
+data_inibica.loc[(data_inibica['IHQ_Luminal_A'] == '0') & (data_inibica['IHQ_Luminal_B'] == '0') &
+                 (data_inibica['IHQ_Her2'] == '0') & (data_inibica['IHQ_Basal'] == '0'),'IHQ_Normal'] = 1
+
+""" Se eliminan las columnas de IHQ, ya que no nos sirven """
+data_inibica = data_inibica.drop(['ER', 'PR', 'Ki-67', 'Her-2'], axis = 1)
+
+""" Se eliminan las columnas de mutaciones, ya que no sirven en este caso """
+data_inibica = data_inibica[data_inibica.columns.drop(list(data_inibica.filter(regex='SNV|CNV')))]
+
+""" Se ordenan las columnas de igual manera en el que fueron colocadas durante el proceso de entrenamiento. """
+cols = data_inibica.columns.tolist()
+cols = cols[:2] + cols[6:7] + cols[2:3] + cols[5:6] + cols[3:5] + cols[7:]
+data_inibica = data_inibica[cols]
+
+#data_inibica.to_excel('inference_inibica_clinical-anatomopathologic.xlsx')
+
+""" Se carga el Excel de nuevo ya que anteriormente se ha guardado """
+data_inibica_complete = pd.read_excel('/home/avalderas/img_slides/correlations/clinical-anatomopathologic/inference/test_data&models/inference_inibica_clinical-anatomopathologic.xlsx',
+                                      engine='openpyxl')
+
+""" Ahora habria que eliminar la columna de pacientes y dividir las columnas en entradas y salidas de tipo histológico,
+estadio anatomopatológico, pT, pN, pM e IHQ. """
+data_inibica_complete = data_inibica_complete.drop(['Paciente'], axis = 1)
+
+test_tabular_data = data_inibica_complete.iloc[:, :6]
+
+test_labels_tumor_type = data_inibica_complete.iloc[:, 6:12]
+test_labels_STAGE = data_inibica_complete.iloc[:, 12:21]
+test_labels_pT = data_inibica_complete.iloc[:, 21:30]
+test_labels_pN = data_inibica_complete.iloc[:, 30:40]
+test_labels_pM = data_inibica_complete.iloc[:, 40:43]
+test_labels_IHQ = data_inibica_complete.iloc[:, 43:]
+
+""" Para poder realizar la inferencia hace falta transformar los dataframes en arrays de numpy. """
+test_tabular_data = np.asarray(test_tabular_data).astype('float32')
+
+test_labels_tumor_type = np.asarray(test_labels_tumor_type).astype('float32')
+test_labels_STAGE = np.asarray(test_labels_STAGE).astype('float32')
+test_labels_pT = np.asarray(test_labels_pT).astype('float32')
+test_labels_pN = np.asarray(test_labels_pN).astype('float32')
+test_labels_pM = np.asarray(test_labels_pM).astype('float32')
+test_labels_IHQ = np.asarray(test_labels_IHQ).astype('float32')
+
+""" Una vez ya se tienen las entradas y las tres salidas correctamente en formato numpy, se carga el modelo de red para
+realizar la inferencia. """
 model = load_model('/home/avalderas/img_slides/correlations/clinical-anatomopathologic/inference/test_data&models/clinical-anatomopathologic.h5')
 
-test_tabular_data = np.load('/home/avalderas/img_slides/correlations/clinical-anatomopathologic/inference/test_data&models/test_data.npy')
-
-test_labels_tumor_type = np.load('/home/avalderas/img_slides/correlations/clinical-anatomopathologic/inference/test_data&models/test_labels_tumor_type.npy')
-test_labels_STAGE = np.load('/home/avalderas/img_slides/correlations/clinical-anatomopathologic/inference/test_data&models/test_labels_STAGE.npy')
-test_labels_pT = np.load('/home/avalderas/img_slides/correlations/clinical-anatomopathologic/inference/test_data&models/test_labels_pT.npy')
-test_labels_pN = np.load('/home/avalderas/img_slides/correlations/clinical-anatomopathologic/inference/test_data&models/test_labels_pN.npy')
-test_labels_pM = np.load('/home/avalderas/img_slides/correlations/clinical-anatomopathologic/inference/test_data&models/test_labels_pM.npy')
-test_labels_IHQ = np.load('/home/avalderas/img_slides/correlations/clinical-anatomopathologic/inference/test_data&models/test_labels_IHQ.npy')
-
-""" Una vez entrenado el modelo, se puede evaluar con los datos de test y obtener los resultados de las métricas
-especificadas en el proceso de entrenamiento. En este caso, se decide mostrar los resultados de la 'loss', la exactitud,
-la sensibilidad y la precisión del conjunto de datos de validación."""
+""" Se evalua los pacientes del INiBICA con los datos de test y se obtienen los resultados de las distintas métricas. """
 # @evaluate: Devuelve el valor de la 'loss' y de las métricas del modelo especificadas.
 results = model.evaluate(test_tabular_data, [test_labels_tumor_type, test_labels_STAGE, test_labels_pT, test_labels_pN,
                                              test_labels_pM, test_labels_IHQ], verbose = 0)
@@ -94,12 +284,7 @@ if results[51] > 0 or results[52] > 0:
                                                                                     (results[51] + results[52])))
 
 """ Además, se realiza la matriz de confusión sobre todo el conjunto del dataset de test para evaluar la precisión de la
-red neuronal y saber la cantidad de falsos positivos, falsos negativos, verdaderos negativos y verdaderos positivos.
-
-Para empezar, se itera sobre todas las verdades fundamentales de cada paciente para cada parámetro (pT, IHQ, pN, etc.), 
-para almacenar cual es el índice más alto del vector fila en cada paciente, y por tanto, cual es la clase predominante. 
-Una vez hecho esto, se comparan los índices más altos de las verdades fundamentales y de las predicciones para realizar
-la matriz de confusión para cada parámetro. """
+red neuronal y saber la cantidad de falsos positivos, falsos negativos, verdaderos negativos y verdaderos positivos. """
 # Tipo histológico
 y_true_tumor_type = []
 for label_test_tumor_type in test_labels_tumor_type:
@@ -194,7 +379,7 @@ def plot_confusion_matrix(cm, classes, normalize=False, title='Matriz de confusi
     plt.xlabel('Predicción')
 
 np.set_printoptions(precision=2)
-fig1 = plt.figure(figsize=(7,6))
+fig1 = plt.figure()
 
 plot_confusion_matrix(matrix_tumor_type, classes = matrix_tumor_type_classes, title = 'Matriz de confusión del tipo '
                                                                                       'histológico')
