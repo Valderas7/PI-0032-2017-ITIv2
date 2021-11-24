56167eec7a796ad1adb1eb1662bf7c87f51266b5
Valderas7 <valderas999@hotmail.com>
2021-11-24 16:07:13 +0100

    Modelo nº 49 ejecutado y capturado en pantallas y mapas de calor.

--- mutations/image/inference/inference/inibica_mutations_inference.py
+++ mutations/image/inference/inference/inibica_mutations_inference.py
@@ -58,7 +58,7 @@ alto = 210
 canales = 3
 
 """ Se carga el modelo de la red neuronal """
-path = '/home/avalderas/img_slides/mutations/image/inference/models/model_image_mutations_28_0.45.h5'
+path = '/home/avalderas/img_slides/mutations/image/inference/models/model_image_mutations_49_0.61.h5'
 model = load_model(path)
 epoch_model = path.split('_')[4]
 
